35ea6cfa55cd1bad9464f38ba0235f84020e0ec8
NicolasB_ECE <nicolas.barbaste@edu.ece.fr>
2016-10-14 11:48:36 +0200

    Add a beginning of answer for the third question of the TP2

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This repository contains the .java for the 3 questions of the TP2
 
 * Exo1.java -> Answer the first question of the TP2 [Count first name by origin]
 * Exo2.java -> Answer the second question of the TP2 [Count number of first name by number of origin (how many first name has x origins ? For x = 1,2,3...)]
+* Exo3.java -> Answer the third question of the TP2 [Proportion (in%) of male or female] => I've only done the first part of this question, which counts the number of female and male names using M/R
 
 (WordCount.java is the example used to answer the questions)
 
---
+++ src/main/java/Exo3.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Exo3 {
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            String row = new String(value.toString()); //Put the Text value into a String
+            String[] rowArray = row.split(";"); //Split the String "row" into an array, using ";" as a spliter
+            String[] genresArray = rowArray[1].replaceAll(", ", ",").split(","); //Take the String which represents the gender(s) of the name and split it into an array, using "," as a spliter
+
+            for(String genre : genresArray) //For each gender, write it into the context
+            {
+                word.set(genre);
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Exo 3");
+        job.setJarByClass(Exo3.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
