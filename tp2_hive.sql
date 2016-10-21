df32fc4378fa0555be89d9a57c7647c7bc5e99bf
Nicolas B <nicolas.barbaste@edu.ece.fr>
2016-10-21 13:43:24 +0200

    Edit .sql

--- tp2_hive.sql
+++ tp2_hive.sql
@@ -6,7 +6,10 @@ CREATE EXTERNAL TABLE nbarbaste.prenoms ( prenom STRING, gender ARRAY<STRING>,or
 ROW FORMAT DELIMITED FIELDS TERMINATED BY '\073' COLLECTION ITEMS TERMINATED by ',' STORED AS TEXTFILE LOCATION '/user/nbarbaste/prenoms';
 
 Exo 1 :
-SELECT origin, COUNT(prenom) FROM prenoms GROUP BY origin;
+SELECT origins, COUNT(prenom) 
+FROM prenoms 
+LATERAL VIEW EXPLODE (origin) originsList AS origins 
+GROUP BY origins;
 
 Exo 2 : 
 SELECT SIZE(origin), COUNT(prenom) FROM prenoms GROUP BY SIZE(origin);
